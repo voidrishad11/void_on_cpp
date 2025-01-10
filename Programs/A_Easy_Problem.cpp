634e22ae8fb0478667bd59f32011832c13abac1c
Md. Rishad Hossain <rishadmahmud11@gmail.com>
2025-01-11 05:25:06 +0600

    11-01-25

---
+++ Programs/A_Easy_Problem.cpp
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------
+         author    : voidrishad11
+         created   : Saturday | 11 January, 2025 | 05:14:10 GMT +6
+---------------------------------------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define mii map<int, int>
+#define si set<int>
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define nL "\n"
+#define pb push_back
+#define mk make_pair
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int main() {
+    fast;
+    ll tc;
+    cin >> tc;
+    while(tc--) 
+    {
+        ll n;
+        cin >> n;
+        cout << n-1 << nL;
+    }
+    return 0;
+}
